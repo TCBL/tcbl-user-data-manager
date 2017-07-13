3caed1f2fc15f0caec322a6e033350eee3afeb3e
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2017-07-13 15:29:48 +0200

    Made a separate UserController. This will be responsible for managing user data

---
+++ src/main/java/be/ugent/idlab/tcbl/userdatamanager/controller/UserController.java
@@ -0,0 +1,63 @@
+package be.ugent.idlab.tcbl.userdatamanager.controller;
+
+import be.ugent.idlab.tcbl.userdatamanager.model.TCBLUser;
+import be.ugent.idlab.tcbl.userdatamanager.model.TCBLUserRepository;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+/**
+ * <p>Copyright 2017 IDLab (Ghent University - imec)</p>
+ *
+ * @author Gerald Haesendonck
+ */
+@Controller
+public class UserController {
+
+	private WebClient webClient = WebClient.create();
+
+	private final TCBLUserRepository tcblUserRepository;
+
+	public UserController(TCBLUserRepository tcblUserRepository) {
+		this.tcblUserRepository = tcblUserRepository;
+	}
+
+	@RequestMapping("/userinfo")
+	public String userinfo(Model model, OAuth2AuthenticationToken authentication) {
+		Map userAttributes = this.webClient
+				.filter(oauth2Credentials(authentication))
+				.get()
+				.uri(authentication.getClientRegistration().getProviderDetails().getUserInfoUri())
+				.retrieve()
+				.bodyToMono(Map.class)
+				.block();
+		String userName = userAttributes.get("user_name").toString();
+		TCBLUser tcblUser = tcblUserRepository.find(userName);
+		if (tcblUser == null) {
+			tcblUser = new TCBLUser();
+			tcblUser.setUserName(userName);
+		}
+		model.addAttribute("userAttributes", userAttributes);
+		model.addAttribute("tcblUser", tcblUser);
+		return "userinfo";
+	}
+
+	private ExchangeFilterFunction oauth2Credentials(OAuth2AuthenticationToken authentication) {
+		return ExchangeFilterFunction.ofRequestProcessor(
+				clientRequest -> {
+					ClientRequest authorizedRequest = ClientRequest.from(clientRequest)
+							.header(HttpHeaders.AUTHORIZATION, "Bearer " + authentication.getAccessToken().getTokenValue())
+							.build();
+					return Mono.just(authorizedRequest);
+				});
+	}
+
+}
