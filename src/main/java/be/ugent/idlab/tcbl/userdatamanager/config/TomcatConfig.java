14da730195a6d5ff94fbd74881059ec57a52bd41
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2017-08-10 13:20:50 +0200

    Got it working locally. Now parametrize configuration...

--- src/main/java/be/ugent/idlab/tcbl/userdatamanager/config/TomcatConfig.java
+++ src/main/java/be/ugent/idlab/tcbl/userdatamanager/config/TomcatConfig.java
@@ -3,23 +3,27 @@ package be.ugent.idlab.tcbl.userdatamanager.config;
 import org.apache.catalina.connector.Connector;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.autoconfigure.AutoConfigureBefore;
-import org.springframework.boot.autoconfigure.AutoConfigureOrder;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.web.ServerProperties;
-import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
 
 /**
  * See https://github.com/spring-projects/spring-boot/issues/1647
+ *
+ * Adds an AJP connector (and thus endpoint) to the embedded Tomcat servlet container.
+ * This enables efficient cooperation with the Apache HTTP server configured as reverse proxy.
+ *
+ * 
+ * <p>Copyright 2017 IDLab (Ghent University - imec)</p>
+ *
+ * @author Gerald Haesendonck
  */
 @Configuration
-@AutoConfigureBefore(ServletWebServerFactoryAutoConfiguration.EmbeddedTomcat.class)
-@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
+//@AutoConfigureBefore(ServletWebServerFactoryAutoConfiguration.EmbeddedTomcat.class)
+//@AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
 public class TomcatConfig {
 	private Logger log = LoggerFactory.getLogger(this.getClass());
 
@@ -32,9 +36,8 @@ public class TomcatConfig {
 		final Connector ajpConnector = new Connector("AJP/1.3");
 		ajpConnector.setPort(8445);
 		ajpConnector.setScheme("https");
-		ajpConnector.setProxyName("tcblsso2.ilabt.imec.be");
-		ajpConnector.setProxyPort(8445);
-		//ajpConnector.setDomain("");
+		ajpConnector.setProxyName("ravel.elis.ugent.be");
+		ajpConnector.setProxyPort(443);
 		ajpConnector.setSecure(true);
 		tomcat.addAdditionalTomcatConnectors(ajpConnector);
 		log.warn("***** init AJP connector in embedded tomcat done.");
