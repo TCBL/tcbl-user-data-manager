a5d103f89ec06bb77503d7d683c83b7db4f3b8c2
Gerald Haesendonck <gerald.haesendonck@ugent.be>
2018-03-20 13:42:38 +0100

    Refactor statistics: due to usage of "local" database, they can easily be calculated on the fly. The background job is eliminated.

--- src/main/java/be/ugent/idlab/tcbl/userdatamanager/controller/StatController.java
+++ src/main/java/be/ugent/idlab/tcbl/userdatamanager/controller/StatController.java
@@ -1,14 +1,13 @@
 package be.ugent.idlab.tcbl.userdatamanager.controller;
 
 import be.ugent.idlab.tcbl.userdatamanager.model.Stats;
+import be.ugent.idlab.tcbl.userdatamanager.model.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.io.IOException;
 
 /**
  * <p>Copyright 2017 IDLab (Ghent University - imec)</p>
@@ -19,28 +18,38 @@ import java.io.IOException;
 @RequestMapping("stats")
 public class StatController {
 	private final Logger log = LoggerFactory.getLogger(this.getClass());
+	private final UserRepository userRepository;
+
+	public StatController(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
 
 	@GetMapping("/users")
 	public String activeUsers(Model model) {
-		try {
-			Stats stats = Stats.fromLatestFile();
-			if (stats != null) {
-				model.addAttribute("totalCount", stats.totalCount);
-				model.addAttribute("invited", stats.invited);
-				model.addAttribute("invitedActive", stats.invitedActive);
-				model.addAttribute("newUsers", stats.newUsers);
-				model.addAttribute("dates", stats.getLabels());
-				model.addAttribute("active", stats.getActiveValues());
-				model.addAttribute("totalActive", stats.getTotalActiveValues());
-				//model.addAttribute("invitedActive", stats.getInvitedActive());
-			} else {
-				log.warn("Stats not calculated yet.");
-				model.addAttribute("totalCount", null);
+
+			// TODO: sommige users zijn NIET op invitation day erin gestoken. Die van daarvoor tellen ook als "invited"
+		final Stats stats = new Stats();
+		log.info("Calculating statistics");
+		if (stats.exists()) {
+			log.info("Stats already calculated. Skipping");
+		} else {
+			try {
+				userRepository.processTCBLUsers(stats::add);
+				stats.toFile();
+			} catch (Exception e) {
+				log.warn("Calculating statistics went wrong. ", e);
 			}
-		} catch (IOException e) {
-			log.error("Error while getting stats or stats not calculated yet.", e);
-			model.addAttribute("totalCount", null);
 		}
+		log.info("calculating statistics done.");
+		model.addAttribute("totalCount", stats.totalCount);
+		model.addAttribute("invited", stats.invited);
+		model.addAttribute("invitedActive", stats.invitedActive);
+		model.addAttribute("newUsers", stats.newUsers);
+		model.addAttribute("dates", stats.getLabels());
+		model.addAttribute("active", stats.getActiveValues());
+		model.addAttribute("totalActive", stats.getTotalActiveValues());
+		//model.addAttribute("invitedActive", stats.getInvitedActive());
+
 		return "stats/users";
 	}
 
